7e5c80eacaa726abaecf2c2492779a70bd942274
Alex <alejandro.rodriguez.arias@udc.es>
2020-02-20 20:25:31 +0100

    bigotes

--- practica1.m
+++ practica1.m
@@ -2,6 +2,10 @@
 clear all
 
 %% cargamos la base de datos de Iris
+%run loadIris
+%inputs = [VarName1, VarName2, VarName3, VarName4];
+%outputs = Irissetosa;
+%% cargamos la base de datos de cancer
 run loadIris
 inputs = [VarName1, VarName2, VarName3, VarName4];
 outputs = Irissetosa;
@@ -13,16 +17,7 @@ TypeCV = 'KFold';
 k = 10;
 cv = cvpartition(outputs,TypeCV,k);
 %% entrenamos con el discriminante lineal
-for i = 1:cv.NumTestSets
-    trIdx = cv.training(i);
-    teIdx = cv.test(i);
-    mdlListDiscrLinear{i} = fitcdiscr(inputs(trIdx,:), outputs(trIdx,:), 'DiscrimType', typeDiscr);
-    prediction = predict(mdlListDiscrLinear{i}, inputs(teIdx,:));
-    [CM, orderCM] = confusionmat(outputs(teIdx,:), prediction);
-    for j = 1:size(CM,1)
-        [RecallLinear(i,j),SpecLinear(i,j),PrecisionLinear(i,j),NPVLinear(i,j),ACCLinear(i,j),F1ScoreLinear(i,j)] = performance_indexes(CM,j);
-    end
-end
+[RecallLinear,SpecLinear,PrecisionLinear,NPVLinear,ACCLinear,F1ScoreLinear, predictionLinear] = trainingDiscr(typeDiscr, cv, inputs, outputs);
 %% muestra de resultados medios
 fprintf('Precision media para el discriminante lineal con Iris: %f\n',mean(mean(PrecisionLinear)))
 fprintf('Recall media para el discriminante lineal con Iris: %f\n',mean(mean(RecallLinear)))
@@ -32,38 +27,27 @@ fprintf('Spec media para el discriminante lineal con Iris : %f\n',mean(mean(Spec
 
 %% entrenamos con el discriminante cuadratico
 typeDiscr = 'quadratic';
-
-for i = 1:cv.NumTestSets
-    trIdx = cv.training(i);
-    teIdx = cv.test(i);
-    mdlListDiscrQuadr{i} = fitcdiscr(inputs(trIdx,:), outputs(trIdx,:), 'DiscrimType', typeDiscr);
-    prediction = predict(mdlListDiscrQuadr{i}, inputs(teIdx,:));
-    [CM, orderCM] = confusionmat(outputs(teIdx,:), prediction);
-    for j = 1:size(CM,1)
-        [RecallQuadr(i,j),SpecQuadr(i,j),PrecisionQuadr(i,j),NPVQuadr(i,j),ACCQuadr(i,j),F1ScoreQuadr(i,j)] = performance_indexes(CM,j);
-    end
-end
+[RecallQuadr,SpecQuadr,PrecisionQuadr,NPVQuadr,ACCQuadr,F1ScoreQuadr, predictionQuadr] = trainingDiscr(typeDiscr, cv, inputs, outputs);
 %% muestra de resultados medios
 fprintf('Precision media para el discriminante cuadratico con Iris: %f\n',mean(mean(PrecisionQuadr)))
 fprintf('Recall media para el discriminante cuadratico con Iris: %f\n',mean(mean(RecallQuadr)))
 fprintf('ACC media para el discriminante cuadratico con Iris: %f\n',mean(mean(ACCQuadr)))
 fprintf('Spec media para el discriminante cuadratico con Iris : %f\n',mean(mean(SpecQuadr)))
 
-
-%% Curvas ROC
-
+%% Curva ROC
+%[X,Y,T,AUC] = perfcurve(outputs{cv.test(1)},prediction{1},'virginica');
+%plot(X,Y)
 %% Errores de entrenamiento y test
 
 %% Diagramas de cajas y bigotes
-
-%% Diferencias significativas entre modelos
 ACCMeanLinear = mean(ACCLinear,2); % para cada modelo calculamos la ACC media para las tres clases de flor
 ACCMeanQuadr = mean(ACCQuadr,2);
+boxplot([ACCMeanLinear,ACCMeanQuadr])
+
+%% Diferencias significativas entre modelos
 muestras = [ACCMeanLinear,ACCMeanQuadr];
 etiquetas = ['linear';'quadra'];
 [P] = testEstadistico(muestras,etiquetas,0.05)
-
-
 
 
 
