c9c15a3b365245b05b288797a609a0d34ae5b85a
Alex <alejandro.rodriguez.arias@udc.es>
2020-02-27 18:22:18 +0100

    if para diferenciar casos con solo dos clases de los casos con multiples clases

--- trainingDiscr.m
+++ trainingDiscr.m
@@ -7,9 +7,13 @@ for i = 1:cv.NumTestSets
     mdlListDiscrLinear{i} = fitcdiscr(inputs(trIdx,:), outputs(trIdx,:), 'DiscrimType', typeDiscr);
     prediction{i} = predict(mdlListDiscrLinear{i}, inputs(teIdx,:));
     [CM, orderCM] = confusionmat(outputs(teIdx,:), prediction{i});
-    for j = 1:size(CM,1)
-        [Recall(i,j),Spec(i,j),Precision(i,j),NPV(i,j),ACC(i,j),F1Score(i,j)] = performance_indexes(CM,j);
-    end
+    if size(CM,1) > 2
+        for j = 1:size(CM,1)
+            [Recall(i,j),Spec(i,j),Precision(i,j),NPV(i,j),ACC(i,j),F1Score(i,j)] = performance_indexes(CM,j);
+        end
+    else
+        [Recall(i),Spec(i),Precision(i),NPV(i),ACC(i),F1Score(i)] = performance_indexes(CM,2); 
+    end    
 end
 
 end
