223d7c3065bcaaa664cc709636d6b6d8da1407fc
Pablo Aja Macaya <paabloaja@gmail.com>
2020-03-10 19:06:29 +0100

    trainingTree function
    Primera aproximación para el entrenamiento de árboles

---
+++ trainingTree.m
@@ -0,0 +1,10 @@
+function mdlListDiscrLinear = trainingTree(cv, inputs, outputs, MaxNumSplitsValue, MinLeafSizeValue, MinParentSizeValue, MergeLeavesValue)
+%% entrenamos con el discriminante
+for i = 1:cv.NumTestSets
+    trIdx = cv.training(i);
+    mdlListDiscrLinear{i} = fitctree(inputs(trIdx,:), outputs(trIdx,:),'MaxNumSplits', cv.TrainSize(i)-1, 'MinLeafSize', MinLeafSizeValue, 'MinParentSize', MinParentSizeValue, 'MergeLeaves', MergeLeavesValue);
+end
+end
+
+% , PredictorNamesValue, ResponseNameValue, SplitCriterionValue
+% , 'PredictorNames', PredictorNamesValue, 'ResponseName', ResponseNameValue, 'SplitCriterion', SplitCriterionValue
