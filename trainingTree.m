efb99b5f32ff84b0ef47b75c332ff2be050302dc
Pablo Aja Macaya <paabloaja@gmail.com>
2020-03-11 16:41:11 +0100

    Añadido SplitCriterion a trainingTree

--- trainingTree.m
+++ trainingTree.m
@@ -1,8 +1,8 @@
-function mdlListDiscrLinear = trainingTree(cv, inputs, outputs, MaxNumSplitsValue, MinLeafSizeValue, MinParentSizeValue, MergeLeavesValue, InputsNames)
+function mdlListDiscrLinear = trainingTree(cv, inputs, outputs, MaxNumSplitsValue, MinLeafSizeValue, MinParentSizeValue, MergeLeavesValue, InputsNames, SplitCriterionValue)
 %% entrenamos con el discriminante
 for i = 1:cv.NumTestSets
     trIdx = cv.training(i);
-    mdlListDiscrLinear{i} = fitctree(inputs(trIdx,:), outputs(trIdx,:),'MaxNumSplits', MaxNumSplitsValue, 'MinLeafSize', MinLeafSizeValue, 'MinParentSize', MinParentSizeValue, 'MergeLeaves', MergeLeavesValue,'PredictorNames',InputsNames);
+    mdlListDiscrLinear{i} = fitctree(inputs(trIdx,:), outputs(trIdx,:),'MaxNumSplits', MaxNumSplitsValue, 'MinLeafSize', MinLeafSizeValue, 'MinParentSize', MinParentSizeValue, 'MergeLeaves', MergeLeavesValue,'PredictorNames',InputsNames, 'SplitCriterion', SplitCriterionValue);
 end
 end
 
